d95dee9a541465558f7c23a3222ee92e37bdd286
NotAShelf <raf@notashelf.dev>
2026-05-01 15:50:53 +0300

    nix: get package version from virtual manifest
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: Ie6c1e2fcc4c9bbaffc594689d5bbce426a6a6964

--- nix/package.nix
+++ nix/package.nix
@@ -1,31 +1,34 @@
 {
   lib,
   rustPlatform,
-}:
-rustPlatform.buildRustPackage (finalAttrs: {
-  pname = "rom";
-  version = "0.1.0";
+}: let
+  cargoTOML = (lib.importTOML ../Cargo.toml).workspace.package;
+in
+  rustPlatform.buildRustPackage {
+    pname = "rom";
+    version = cargoTOML.version;
 
-  src = let
-    fs = lib.fileset;
-    s = ../.;
-  in
-    fs.toSource {
-      root = s;
-      fileset = fs.unions [
-        (s + /crates)
-        (s + /rom)
-        (s + /Cargo.lock)
-        (s + /Cargo.toml)
-      ];
+    src = let
+      fs = lib.fileset;
+      s = ../.;
+    in
+      fs.toSource {
+        root = s;
+        fileset = fs.unions [
+          (s + /crates)
+          (s + /rom)
+          (s + /Cargo.lock)
+          (s + /Cargo.toml)
+        ];
+      };
+
+    cargoLock.lockFile = ../Cargo.lock;
+    enableParallelBuildingByDefault = true;
+
+    meta = {
+      description = "Pretty build graphs for your pretty Nix builds";
+      homepage = "https://github.com/feel-co/rom";
+      maintainers = with lib.maintainers; [NotAShelf];
+      license = lib.licenses.eupl12;
     };
-
-  cargoLock.lockFile = ../Cargo.lock;
-  enableParallelBuildingByDefault = true;
-
-  meta = {
-    description = "Pretty build graphs for your pretty Nix builds";
-    maintainers = with lib.maintainers; [NotAShelf];
-    license = lib.licenses.eupl12;
-  };
-})
+  }
